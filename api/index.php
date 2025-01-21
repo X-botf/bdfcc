b087a6708fdb56ea008bd3b6e058165672cc9874
X-botf <168183930+X-botf@users.noreply.github.com>
2025-01-20 20:12:36 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -7,14 +7,14 @@
 <script defer src="/_vercel/insights/script.js"></script>
 	<title>Summit CU</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
-	<link href="https://my.solaritycu.org/Themes/SolarityFCUTheme/Styles/Default_9MqA4nxIt-o1_Default.bndl?v=yzNdlRdA7lAjxNw1K3da678CDug0DPRX1b8iWHvm6Hs1" rel="stylesheet" type="text/css" />
-	<link href="https://my.solaritycu.org/Themes/SolarityFCUTheme/Styles/Default_9MqA4nxIt-o1_Default.bndl?v=yzNdlRdA7lAjxNw1K3da678CDug0DPRX1b8iWHvm6Hs1" rel="stylesheet" type="text/css" /><meta content="Orchard" name="generator" /><meta content="UL7b2EHVJtmrlRT1M0TmyQL0f-m08XynqkgbmVIb6R0_MaQrmwWxUkHdhEl5Lah743jtUufOZRi0GPzX3jTtfX_aRjDpg6zPBjNmpYctzPWa5_Xsp-a2XBm4gI_6EwT2bIPDdoL5IurcvSAxQaEGsLTYarndvw48DXLNOM4Efco1" name="csrf-token" /><meta content="X-AntiCSRF-Token" name="csrf-param" /><meta content="__AntiCsrfToken" name="csrf-form-param" />
-	<link href="https://my.solaritycu.org/Media/Default/Favicon/favicon.png" rel="shortcut icon" type="image/x-icon" /><meta name="apple-itunes-app" content="app-id=584555550">
+	<link href="https://access.widgetfinancial.com/Themes/WidgetFCUTheme/Styles/Default_2tIu2Br4pGU1_Default.bndl?v=9usxLJemQTt01phIyHSe3YnaK1l5bbqHcio4V9Ni61E1" rel="stylesheet" type="text/css" />
+	<link href="https://access.widgetfinancial.com/Themes/WidgetFCUTheme/Styles/Default_2tIu2Br4pGU1_Default.bndl?v=9usxLJemQTt01phIyHSe3YnaK1l5bbqHcio4V9Ni61E1" rel="stylesheet" type="text/css" /><meta content="Orchard" name="generator" /><meta content="UL7b2EHVJtmrlRT1M0TmyQL0f-m08XynqkgbmVIb6R0_MaQrmwWxUkHdhEl5Lah743jtUufOZRi0GPzX3jTtfX_aRjDpg6zPBjNmpYctzPWa5_Xsp-a2XBm4gI_6EwT2bIPDdoL5IurcvSAxQaEGsLTYarndvw48DXLNOM4Efco1" name="csrf-token" /><meta content="X-AntiCSRF-Token" name="csrf-param" /><meta content="__AntiCsrfToken" name="csrf-form-param" />
+	<link href="https://my.solaritycu.org/Media/Default/Favicon/favicon.png" rel="shortcut icon" type="image/x-icon" /><meta name="apple-itunes-app" content="app-id=512153486">
 </head>
 <body><script> window.Chorus = window.Chorus || {}; window.Chorus.isMobile = true; window.Chorus.isMfmHosted = false; window.Chorus.isIFrameHosted = false; </script><script id="boot-data" type="application/json"> {"AvaliableFeatures":{"Features":[{"key":"Login\\Behavior\\QuickPassThroughUserNameStep","value":false},{"key":"Login\\Behavior\\RedirectToFiSiteAfterLogoff","value":true},{"key":"Login\\BiometricAuthentication\\Enabled","value":true},{"key":"Login\\MobileInterrupter\\Enabled","value":false},{"key":"Applications\\DataSource\\Accounts\\UiSettings\\NumericAccountNumberEnabled","value":true},{"key":"Applications\\DataSource\\Accounts\\UiSettings\\AccountTypeIconEnabled","value":true}]},"DisplayMode":{"IsMobile":true,"DisplayModeId":"Mobile"}} </script>
 <header class="navbar" role="banner">
 <div class="navbar__center">
-<div class="top-logo-block" style="text-align: center; display: block; width: 100%; margin: 10px auto;"><a href="/"><img alt="Summit Credit Union" height="80" src="https://my.solaritycu.org/Media/Default/FILogo/SolarityMobileLogo140x30.png" width="186" /> </a></div>
+<div class="top-logo-block" style="text-align: center; display: block; width: 100%; margin: 10px auto;"><a href="/"><img alt="Summit Credit Union" height="80" src="https://www.widgetfinancial.com/wp-content/uploads/2020/01/WidgetLogo@2x-1.png" width="186" /> </a></div>
 </div>
 </header>
     
